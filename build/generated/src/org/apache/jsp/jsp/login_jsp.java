ef0c114a3df5faddaec47d09c472491f2cee1994
agusastra <49885118+agusastra@users.noreply.github.com>
2020-10-20 20:15:19 +0200

    Add files via upload

---
+++ build/generated/src/org/apache/jsp/jsp/login_jsp.java
@@ -0,0 +1,80 @@
+package org.apache.jsp.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Page Login</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <form method=\"post\" action=\"login.jsp\">\n");
+      out.write("            <fieldset style=\"background-color: gainsboro\">\n");
+      out.write("                <legend>Connexion</legend>\n");
+      out.write("                <h1>Bonjour, voulez rentrer vos identifiants !</h1>\n");
+      out.write("                <pre>\n");
+      out.write("          Nom          : <input type=\"text\" id=\"name\" name=\"name\" style=\"width: 150px\" ><br>\n");
+      out.write("          Mot de passe : <input type=\"password\" id=\"motdepasse\" name=\"motdepasse\" minlength=\"8\" style=\"width: 150px\"  required>\n");
+      out.write("          (8 characters minimum)<br>\n");
+      out.write("                         <input type=\"submit\" value=\"Valide\" >    <input type=\"reset\" value=\"Effacer\">\n");
+      out.write("                </pre>\n");
+      out.write("            </fieldset>\n");
+      out.write("        </form>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
